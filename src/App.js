5817505198032b34ada69edff77a20159011f465
JONGHYUN YOON <jonghyunyoon@JONGHYUNui-MacBook-Pro.local>
2020-06-23 21:21:57 +0900

    2020.06.23.Tue -m "15min study"

--- src/App.js
+++ src/App.js
@@ -5,20 +5,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <div className="nav-wrapper">
+          <div>Learn React.js</div>
+        </div>
+      </nav>
+      <h1>Material CSS</h1>
     </div>
   );
 }
--- src/stories/InputStory.jsx
+++ src/stories/InputStory.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 
 import Input from '../03/Input';
+import { bindActionCreators } from 'redux';
 
 storiesOf('Input', module)
-  .add('기본 설정', () => <Input name="name" />)
-  .add('label 예제', () => <Input name="name" label="이름" />);
+  .addWithJSX('기본 설정', () => <Input name="name" />)
+  .addWithJSX('label 예제', () => <Input name="name" label="이름" />)
+  .addWithJSX('onChange 예제', () => <Input name="name" onChange={action('onChange Event')} />);
