b73fcff7b617aaca0aea012b61fd59e9d83de9d4
JONGHYUN YOON <jonghyunyoon@JONGHYUNui-MacBook-Pro.local>
2020-07-07 20:55:03 +0900

    2020.07.06

---
+++ src/03/JSXSample.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class JSXSample extends Component {
+  render() {
+    return (
+      // 아래는 JSX 포맷입니다.
+      <div>
+        <img src="http://www.easyspub.co.kr/images/logo_footer.png" />
+        <div>Hello!</div>
+      </div>
+    );
+  }
+}
+
+export default JSXSample;
--- src/App.js
+++ src/App.js
@@ -4,13 +4,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <nav>
-        <div className="nav-wrapper">
-          <div>Learn React.js</div>
-        </div>
-      </nav>
-      <h1>Material CSS</h1>
+    <div>
+      <img src="http://www.easyspub.co.kr/images/logo_footer.png" />
+      <div>Hello!</div>
     </div>
   );
 }
